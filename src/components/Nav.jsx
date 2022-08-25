82e06c387da95c905d2eb8985af3fa37cba95150
Olivers11 <olstertecn597@gmail.com>
2022-08-24 23:03:42 -0600

    navbar working and before at new build

---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import './../styles/Nav.css';
+
+
+
+export default function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <a className="navbar-brand" href="/">MDental <i className="fas fa-tooth"></i> </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" 
+	data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
+	aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Horarios</a>
+          </li>
+	  <li className="nav-item">
+            <a className="nav-link" href="#">Sevicios</a>
+          </li>
+	  <li className="nav-item">
+            <a className="nav-link" href="#">Citas</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  );
+}
