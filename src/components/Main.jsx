b109aecf58152c637f80aa5defa1f775e6648f70
Olivers11 <olstertecn597@gmail.com>
2022-08-30 17:11:51 -0600

    before that change style from date page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,17 @@
 import './../styles/main.css';
 import Separator from './Separator';
+import Cita from './Cita';
 
 export default function Main(){
   return(
-    <div className="img-container">
-      <h2 className='text-center text-white title'> <b id="title-name">MDental</b> el trato que necesitas</h2> 
-      <button className="btn" id="btn-cita">Hacer Cita</button>
-      <Separator  />
-    </div>
+    <>
+      <div className="img-container">
+	<h2 className='text-center text-white title'> <b id="title-name">MDental</b> el trato que necesitas</h2> 
+	<a href="#cita" className="btn" id="btn-cita">Hacer Cita</a>
+	<Separator  />
+      </div>
+      <Cita />
+
+    </>
   );
 }
